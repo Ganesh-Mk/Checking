bc72014ee7a0734c313d647ead4c0d96fe3a79e2
Anwar <anwarimambhai@gmail.com>
2024-05-07 12:14:02 +0530

    Hello

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,7 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
   const [name, setName] = useState("");
-  const [head, setHead] = useState("Hi");
+  const [head, setHead] = useState("Hello");
 
   const handleSubmit = () => {
     axios
